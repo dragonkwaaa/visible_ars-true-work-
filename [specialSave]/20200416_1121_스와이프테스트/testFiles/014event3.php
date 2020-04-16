6f72100b9eaf9496167bbc4c764909e726d974ba
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-04-16 11:28:22 +0900

    Create 014event3.php

---
+++ [specialSave]/20200416_1121_스와이프테스트/testFiles/014event3.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<!-- <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> -->
+
+<link rel="stylesheet" href="/common/css/jquery.mobile-1.4.5.css" />
+<script src="/common/js/jquery-3.1.1.min.js"></script>
+<script src="/common/js/jquery-mobile-1.5.0.js"></script>
+
+
+<script src="customSwipePage.js"></script>
+
+<title>jquery mobile 3</title>
+</head>
+
+<body>
+<section data-role="page" id="2.swipe flick3" class="eventPage" data-next="014event" data-prev="014event2" data-dom-cache="true">
+    <header data-role="header">
+        <h1>jQuery Mobile 3</h1>
+    </header>
+    
+    <div class="content" data-role="content"><h1>좌우로 밀어주세요3</h1></div>
+    <div class="content" data-role="content"><h1>좌우로 밀어주세요3</h1></div>
+    <div class="content" data-role="content"><h1>좌우로 밀어주세요3</h1></div>
+
+    <!-- <footer data-role="footer"><h1>drsggg.tistory.com</h1></footer> -->
+</section>
+</body>
+
+</html>
