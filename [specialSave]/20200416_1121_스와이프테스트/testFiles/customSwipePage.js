104a3c910ce947b2bd00b7f6ea3da549ac9a739d
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-04-16 11:28:58 +0900

    Create customSwipePage.js

---
+++ [specialSave]/20200416_1121_스와이프테스트/testFiles/customSwipePage.js
@@ -0,0 +1,47 @@
+// swipe
+// $( document ).one( "pagecreate", ".eventPage", function() {
+
+$( document ).one( "pagecreate", ".container.main", function() {
+
+    // Handler for navigating to the next page
+    function navnext( next ) {
+        $( ":mobile-pagecontainer" ).pagecontainer( "change", next + ".php", {
+            transition: "slide"
+            });
+        }
+ 
+        // Handler for navigating to the previous page
+    function navprev( prev ) {
+        $( ":mobile-pagecontainer" ).pagecontainer( "change", prev + ".php", {
+            transition: "slide",
+            reverse: true
+        });
+    }
+    
+    $( document ).on( "swipeleft", ".ui-page", function( event ) {
+        var next = $( this ).jqmData( "next" );
+        if ( next ) {
+            navnext( next );
+        }
+    });
+    
+    $( document ).on( "swiperight", ".ui-page", function( event ) {
+        var prev = $( this ).jqmData( "prev" );
+        if ( prev ) {
+            navprev( prev );
+        }
+    });
+    
+});
+
+
+
+
+
+// :: 버튼 눌렀을 때의 슬라이드 
+	function navprev( prev ) {
+        $( ":mobile-pagecontainer" ).pagecontainer( "change", prev + ".php", {
+            transition: "slide",
+            reverse: true
+        });
+    }
