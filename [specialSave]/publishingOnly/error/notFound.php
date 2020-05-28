9be176d079c979e5634a52bbe662f79ef1ba7956
dragonkwaaa <61732954+dragonkwaaa@users.noreply.github.com>
2020-05-28 13:52:19 +0900

    Add files via upload

---
+++ [specialSave]/publishingOnly/error/notFound.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<?php include $_SERVER['DOCUMENT_ROOT'] .  '/common/pages/head.php';
+?>
+
+<body>
+<div class="container main">
+	<div class="contents">
+		
+		<div class="section notFoundSect">
+			<img class="notFoundIcon" src="/common/img/icon_notFound.png">
+			<div class="infoText">
+				잘못된 페이지입니다.</br>
+				불편을 드려서 죄송합니다.
+			</div>
+			<a href="../" class="btn">뒤로가기</a>
+		</div>
+
+
+
+
+	</div>
+</div>
+<script>
+</script>
+</body>
+</html>
